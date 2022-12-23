2e75aa290ce8ea9619d1fd524092b4a83631968a
heyhiru <hirentimbadiya74@gmail.com>
2022-12-23 15:58:00 +0530

    Added Header and Home Components

---
+++ src/Components/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './Header.jsx'
+import Home from './Home.jsx'
+
+function App() {
+  return (
+    <div className='App'>
+      <Header/>
+      <Home/>
+    </div>
+  )
+}
+
+export default App
