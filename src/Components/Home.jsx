f6d868716c69702aee6d22565f85c1c3619a85a5
heyhiru <hirentimbadiya74@gmail.com>
2022-12-27 11:23:40 +0530

    Added text in array and passed as props

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -2,7 +2,14 @@ import React from "react";
 import styled from "styled-components";
 import Section from "./Section";
 
-const text = "Schedule a Demo Drive";
+const text = [
+  "Schedule a Demo Drive",
+  "Lowest Cost Solar Panels in America",
+  "Produce Clean Energy From Your Roof",
+];
+
+const leftBtnTxt = ["Buy Now" , "Order Now" , "Shop Now"];
+const rightBtnTxt = ["Custom Order" , "Learn More"];
 
 const downArrow = "/images/down-arrow.svg";
 function Home() {
@@ -10,55 +17,55 @@ function Home() {
     <Container>
       <Section
         model="Model X"
-        text={text}
-        leftBtn="Buy Now"
-        rightBtn="Custom Order"
+        text={text[0]}
+        leftBtn={leftBtnTxt[0]}
+        rightBtn={rightBtnTxt[0]}
         arrow={downArrow}
         imgsrc="model-x.jpg"
       />
       <Section
         model="Model S"
-        text={text}
-        leftBtn="Buy Now"
-        rightBtn="Custom Order"
+        text={text[0]}
+        leftBtn={leftBtnTxt[0]}
+        rightBtn={rightBtnTxt[0]}
         arrow={downArrow}
         imgsrc="model-s.jpg"
       />
       <Section
         model="Model Y"
-        text={text}
-        leftBtn="Buy Now"
-        rightBtn="Custom Order"
+        text={text[0]}
+        leftBtn={leftBtnTxt[0]}
+        rightBtn={rightBtnTxt[0]}
         arrow={downArrow}
         imgsrc="model-y.jpg"
       />
       <Section
         model="Model 3"
-        text={text}
-        leftBtn="Buy Now"
-        rightBtn="Custom Order"
+        text={text[0]}
+        leftBtn={leftBtnTxt[0]}
+        rightBtn={rightBtnTxt[0]}
         arrow={downArrow}
         imgsrc="model-3.jpg"
       />
       <Section
         model="Solar Panels"
-        text="Lowest Cost Solar Panels in America"
-        leftBtn="Order Now"
-        rightBtn="Learn More"
+        text={text[1]}
+        leftBtn={leftBtnTxt[1]}
+        rightBtn={rightBtnTxt[1]}
         arrow={downArrow}
         imgsrc="solar-panel.jpg"
       />
       <Section
         model="Solar Roof"
-        text="Produce Clean Energy From Your Roof"
-        leftBtn="Order Now"
-        rightBtn="Learn More"
+        text={text[2]}
+        leftBtn={leftBtnTxt[1]}
+        rightBtn={rightBtnTxt[1]}
         arrow={downArrow}
         imgsrc="solar-roof.jpg"
       />
       <Section
         model="Accessories"
-        leftBtn="Shop Now"
+        leftBtn={leftBtnTxt[2]}
         rightBtn=""
         imgsrc="accessories.jpg"
       />
