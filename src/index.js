15c7da57349a401f74f7681575f4770243930fce
heyhiru <hirentimbadiya74@gmail.com>
2023-01-17 21:36:44 +0530

    Added commet

--- src/index.js
+++ src/index.js
@@ -5,7 +5,12 @@ import './index.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App/>
+    <App />
   </React.StrictMode>
 );
 
+/*
+  "homepage": "https://hirentimbadiya.github.io/tesla-clone",
+  "predeploy": "npm run build",
+  "deploy" : "gh-pages -d build"
+*/
